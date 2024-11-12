b832c90c2857767df40acc449410adb7211f2556
PakoHishikawa <paco.consultor.fjha@gmail.com>
2024-11-12 09:12:38 -0300

    commit inicial

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+define('HOST', 'LOCALHOST');
+define('USER', 'root');
+define("PASS", '');
+define('BASE', 'cadastro');
+
+$conn = new mysqli(HOST,USER,PASS,BASE);
+//  A classe mysqli representa a conexão entre o PHP e um banco de dados MySQL. Ela está incluída nas versões 5 e posteriores do PHP
+
+
+
+
+
+
+?>
